0e80b9da3839d9152b519ab5c6267cee1716eea2
hieuphan1912 <hieuphan19122002@gmail.com>
2024-01-06 08:48:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,22 @@ const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+const morgan = require('morgan');
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   console.log(req.headers);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>Anh tệ lắm em ơi </h1></body></html>');
 
-});
+});*/
+
+
 
 const server = http.createServer(app);
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
